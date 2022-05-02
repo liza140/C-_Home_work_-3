1ec76a43039b713f1882cbd0ccedfee1126a4391
liza140 <liza140@mail.ru>
2022-05-02 15:23:59 +0300

    Длина между двумя точками

---
+++ EXX1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+//A (3,6); B (2,1) -> 5,09 
+//A (7,-5); B (1,-1) -> 7,21
+
+double Length(double x1, double y1, double x2, double y2)
+{
+    double deltaX = Math.Abs(x1 - x2);
+    double deltaY = Math.Abs(y1 - y2);
+    double deltaXY = Math.Sqrt((deltaX * deltaX) + (deltaY * deltaY));
+    return deltaXY;
+}
+
+double ax = 7;
+double ay = -5;
+double bx = 1;
+double by = -1;
+
+Console.WriteLine("Длина " + Math.Round(Length(ax, ay, bx, by), 2));
